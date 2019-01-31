f4ce0722de90fb287a7af0cce6cd1585ea07c4e5
Jose Velez <josevelezq7@gmail.com>
2019-01-31 12:21:54 -0800

    Move server.js –> server/serverClient.js

---
+++ challenge_2/server/serverClient.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/api/currency', (req, res, next) => {
+  const currency = 'bpi';
+  console.log(`https://api.coindesk.com/v1/${ currency }/historical/close.json`);
+
+  axios.get(`https://api.coindesk.com/v1/${ currency }/historical/close.json`)
+  .then(({ data }) => {
+    const relevantData = {};  
+    relevantData[currency] = data[currency],
+    relevantData.disclaimer = data.disclaimer;
+
+    res.send({
+      success: true,
+      message: 'Fetched data from Coindesk API.',
+      data: relevantData,
+    });
+  })
+  .catch(() => {
+    res.send({
+      success: false,
+      message: 'Unable to get data from Coindesk API.',
+    });
+  })
+
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${ PORT }`))
